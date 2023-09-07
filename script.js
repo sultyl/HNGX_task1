7e2c663a864b424df4a92d97dc9fbc5faa35a9ac
Sultan Adeleke <70979325+sultyl@users.noreply.github.com>
2023-09-07 15:22:32 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateDateTime() {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+    const currentUTCTime = currentDate.toISOString().substr(11, 12); 
+  
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDayOfWeek;
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTCTime;
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1);
